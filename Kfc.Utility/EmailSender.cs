a9fa928ab910278e721451b5a1e93e5b64c8afc3
seanny <seannytheirish@gmail.com>
2025-10-05 15:20:49 -0500

    Minor Changes

---
+++ Kfc.Utility/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kfc.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // throw new NotImplementedException();
+            return Task.CompletedTask;
+        }
+    }
+}
