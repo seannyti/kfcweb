0f79f08925e8997ee1828bf97422c901655a52fc
seanny <seannytheirish@gmail.com>
2025-10-11 03:07:17 -0500

    Minor Changes

--- Kfc.Models/ApplicationUser.cs
+++ Kfc.Models/ApplicationUser.cs
@@ -10,7 +10,7 @@ namespace Kfc.Models
 {
     public class ApplicationUser:IdentityUser {
         [Required]
-        public int Name { get; set; }
+        public string Name { get; set; }
 
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
