3dc2dc016259d5b84761fd71b95907f6176b11e4
seanny <seannytheirish@gmail.com>
2025-11-20 19:36:16 -0600

    Bundle Bootstrap locally instead of using CDN

--- MyApp.Frontend/src/main.ts
+++ MyApp.Frontend/src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+
+// Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap'
+
 import App from './App.vue'
 import router from './router'
 
